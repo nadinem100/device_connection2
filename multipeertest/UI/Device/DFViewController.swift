58e1c027f0fe22c976afdb42367714a0c63cb741
Andrew Zhao <andrewhzhao@gmail.com>
2021-01-22 15:52:06 -0500

    updated storyboard

--- multipeertest/UI/Device/DFViewController.swift
+++ multipeertest/UI/Device/DFViewController.swift
@@ -11,6 +11,7 @@ class DFViewController: UIViewController {
 
     let service = AdvertisingService(type: "Drugs and Fluids")
     var fluid = "none"
+    var volume = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Drugs and Fluids"
@@ -49,9 +50,16 @@ class DFViewController: UIViewController {
         fluid = "saline"
         //print("saline")
     }
-    @IBAction func administerSelect(_ sender: Any) {
-        print("administering " + fluid)
+    @IBAction func LSelect(_ sender: Any) {
+        volume = 1
     }
+    @IBAction func L2Select(_ sender: Any) {
+        volume = 2
+    }
+    @IBAction func administerSelect(_ sender: Any) {
+        print("administering \(volume)L of \(fluid)")
+    }
+    
     
     @IBAction func signal(_ sender: Any) {
         
